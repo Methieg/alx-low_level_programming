404a308f988d5612f23c47727676965eaed6c0ef
Methieg <gumbomet@gmail.com>
2022-06-08 13:53:33 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: End program
+ */
+
+int main(void)
+{	char string[9] = "_putchar";
+	int n = 0;
+
+	while (n < 9)
+	{
+		_putchar(string[n]);
+		++n;
+	}
+	putchar(11);
+}
+	return (0);
+	}
