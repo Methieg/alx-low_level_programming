a78df51c156659a8ab5f388a68949f1ee79a0acb
Methieg <gumbomet@gmail.com>
2022-06-29 16:36:45 -0700

    staticlib

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * _strlen -when used in the function should give out the length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+int l = 0;
+while (*s != '\0')
+{
+s++;
+l++;
+}
+return (l);
+}
