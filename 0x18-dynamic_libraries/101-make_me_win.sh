04a942a7f5e946af72cc22d3b164de0766d96dd3
Methieg <gumbomet@gmail.com>
2022-09-08 09:25:32 -0700

    task update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Methieg/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
+wget -P /tmp/ https://raw.github.com/Methieg/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
