beef6b15e88f3d42840036ce69e01a5c7dc9df3e
Methieg <gumbomet@gmail.com>
2024-01-14 15:40:41 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-will print name of program,followed by newline
+ * @argc: command line count
+ * @argv: more if a vector array,give or take
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
