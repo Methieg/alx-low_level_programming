58389e1f4a70290303d6338036e0a1b465c112a7
Methieg <gumbomet@gmail.com>
2024-01-14 17:28:01 -0800

    arg count printer

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("Argument number passed : %d\n", argc - 1);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
 
 	return (0);
 }
