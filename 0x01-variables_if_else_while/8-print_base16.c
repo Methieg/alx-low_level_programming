092452644bac4f428bf2c4ff7f1b0b93e5808cf8
Methieg <gumbomet@gmail.com>
2025-03-07 23:38:23 +0200

    base 16 printer

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 16 in lowercase
+ * followed by newline
+ *
+ * Return: (0) - Success
+ */
+int main(void)
+{
+	int i;
+	char hexnumber;
+
+
+	i = 0;
+
+	while (i <= 16)
+	{
+		if (i < 10)
+		{
+			hexnumber = i + '0';
+		}
+		else
+		{
+			hexnumber = (i - 10) + 'a';
+		}
+		putchar(hexnumber);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
