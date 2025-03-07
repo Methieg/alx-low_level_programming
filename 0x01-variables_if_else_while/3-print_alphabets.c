8076ddfb9bbfa74de906d00f974d733a67c2ff26
Methieg <gumbomet@gmail.com>
2025-03-07 21:34:42 +0200

    putchar contd practice

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase,uppercase alphabet
+ * using putchar three times only
+ *
+ * Return: (0) - Success
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
