8220c12543339df61392d8a4dd2f7b3d10a1f469
Methieg <gumbomet@gmail.com>
2025-03-08 12:24:49 +0200

    printing all possible combinations of single digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of
+ * single digits
+ *
+ * Return: (0) -Success
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i <= 8)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 0 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
