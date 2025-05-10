3f2ff6ba30ea35f7167ba514b7eec1c00fce8925
Methieg <gumbomet@gmail.com>
2025-05-11 01:44:37 +0200

    other rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,19 @@
  */
 void rev_string(char *s)
 {
-int len, i, half;
-char temp;
-for (len = 0; s[len] != '\0'; len++)
-;
-i = 0;
-half = len / 2;
-while (half--)
-{
-temp = s[len - i - 1];
-s[len - i - 1] = s[i];
-s[i] = temp;
-i++;
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	length -= 1;
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
 }
-}
+
